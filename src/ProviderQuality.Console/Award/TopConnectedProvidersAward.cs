156e769bff55c98ed86e69fa3b6fb4b40e5a2c1b
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:42:35 -0500

    added TopConnectedProvidersAward class

---
+++ src/ProviderQuality.Console/Award/TopConnectedProvidersAward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class TopConnectedProvidersAward : AwardBase {
+        public override string Name { get { return "Top Connected Providers"; } }
+
+        public TopConnectedProvidersAward(int initialQuality, TimeSpan expirationTime) : base(initialQuality, expirationTime) {
+        }
+    }
+}
