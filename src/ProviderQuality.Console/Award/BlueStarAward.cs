5755b1880baaf3cb5eda3d9133ebeaacffcee21f
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:52:15 -0500

    added BlueStarAward class

---
+++ src/ProviderQuality.Console/Award/BlueStarAward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class BlueStarAward : AwardBase {
+        public override string Name { get { return "Blue Star"; } }
+
+        public BlueStarAward(int initialQuality, TimeSpan expirationTime) : base(initialQuality, expirationTime) {
+        }
+    }
+}
