3448bf449533475b8e6985a97e3fcd67a55884ea
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:29:48 -0500

    added IAward interface

---
+++ src/ProviderQuality.Console/Award/IAward.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public interface IAward {
+        string Name { get; }
+        int Quality { get; }
+
+        bool IsExpired { get; }
+        bool IsNotExpired { get; }
+        int DaysUntilExpiration { get; }
+
+        int ChangeQuality(int step);
+        int IncrementDay();
+    }
+}
