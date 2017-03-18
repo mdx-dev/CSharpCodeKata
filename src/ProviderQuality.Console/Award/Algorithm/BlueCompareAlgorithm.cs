ecabbd147c6c330121f684edde4befc3490e45c9
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 21:14:20 -0500

    added BlueCompareAlgorithm class

---
+++ src/ProviderQuality.Console/Award/Algorithm/BlueCompareAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award.Algorithm
+{
+    public class BlueCompareAlgorithm : IAwardAlgorithm
+    {
+        public IAward Update(IAward award)
+        {
+            if(award.IsExpired)
+            {
+                award.ChangeQuality(-1 * award.Quality);
+                return award;
+            }
+            var qualityStep = 1;
+
+                 if (award.DaysUntilExpiration <= 5 ) qualityStep = 3;
+            else if (award.DaysUntilExpiration <= 10) qualityStep = 2;
+
+            award.ChangeQuality(qualityStep);
+            return award;
+        }
+    }
+}
