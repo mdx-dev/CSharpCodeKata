24d732cc9b03756dcbb7946ab7a89ac796c06a96
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:41:06 -0500

    added AcmePartnerFacilityAward class

---
+++ src/ProviderQuality.Console/Award/AcmePartnerFacilityAward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class AcmePartnerFacilityAward : AwardBase {
+        public override string Name { get { return "ACME Partner Facility"; } }
+
+        public AcmePartnerFacilityAward(int initialQuality, TimeSpan expirationTime) : base(initialQuality, expirationTime) {
+        }
+    }
+}
