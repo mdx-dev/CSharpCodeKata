ad72dcec5c8f32baffa18d6fe115da9b2cec20fb
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:24:36 -0500

    threw away the old implementation as it was too messy to be trusted
    Also deleted the Award class, I'm going to use an Award namespace
    instead

--- src/ProviderQuality.Tests/UpdateQualityAwardsTests.cs
+++ src/ProviderQuality.Tests/UpdateQualityAwardsTests.cs
@@ -13,23 +13,15 @@ namespace ProviderQuality.Tests
         {
             var app = new Program()
             {
-                Awards = new List<Award>
-                {
-                    new Award {Name = "Blue Distinction Plus", SellIn = 0, Quality = 80}
-                }
             };
 
-            Assert.IsTrue(app.Awards.Count == 1);
-            Assert.IsTrue(app.Awards[0].Name == "Blue Distinction Plus");
-            Assert.IsTrue(app.Awards[0].Quality == 80);
+            //Assert.IsTrue(app.Awards.Count == 1);
+            //Assert.IsTrue(app.Awards[0].Name == "Blue Distinction Plus");
+            //Assert.IsTrue(app.Awards[0].Quality == 80);
 
-            app.UpdateQuality();
+            //app.UpdateQuality();
 
-            Assert.IsTrue(app.Awards[0].Quality == 80);
+            //Assert.IsTrue(app.Awards[0].Quality == 80);
         }
-
-        // +++To Do - 1/10/2013: Discuss with team about adding more tests.  Seems like a lot of work for something
-        //                       that probably won't change.  I watched it all in the debugger and know everything works
-        //                       plus QA has already signed off and no one has complained.
     }
 }
