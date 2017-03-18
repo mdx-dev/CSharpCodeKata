77711ce48690f256ff53ba7214cacb251914f06d
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:40:03 -0500

    added GoveQualityPlusAward class

---
+++ src/ProviderQuality.Console/Award/GovQualityPlusAward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class GovQualityPlusAward : AwardBase {
+        public override string Name { get { return "Gov Quality Plus"; } }
+
+        public GovQualityPlusAward(int initialQuality, TimeSpan expirationTime) : base(initialQuality, expirationTime) {
+        }
+    }
+}
