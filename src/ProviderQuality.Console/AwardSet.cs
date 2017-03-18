ca5eea27a69d13e5cfb27223ca0b5801129a9972
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-18 14:58:05 -0500

    added AwardSet class

---
+++ src/ProviderQuality.Console/AwardSet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProviderQuality.Console.Award;
+
+namespace ProviderQuality.Console
+{
+    public class AwardSet
+    {
+        public List<IAward> Awards { get; private set; }
+
+        public AwardSet(IEnumerable<IAward> awards)
+        {
+            this.Awards = awards.ToList();
+        }
+
+        public void Update()
+        {
+            foreach(var award in Awards)
+            {
+                award.IncrementDay();
+                award.UpdateAlgorithm.Update(award);
+            }
+        }
+
+        public List<IAward> GetByCutoffQuality(int cutoffQuality)
+        {
+            return this.Awards.Where(award => award.Quality >= cutoffQuality).ToList();
+        }
+
+        public List<IAward> GetExpiring(int daysUntilExpiration)
+        {
+            return this.Awards.Where(award => award.DaysUntilExpiration >= daysUntilExpiration).ToList();
+        }
+
+        public List<IAward> GetExpired()
+        {
+            return this.Awards.Where(award => award.IsExpired).ToList();
+        }
+
+        public List<IAward> GetByType(Type type)
+        {
+            return this.Awards.Where(award => award.GetType() == type).ToList();
+        }
+    }
+}
