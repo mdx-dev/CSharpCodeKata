9e26ecde173d9b4b9fe273814aea490a5ffa6eec
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:51:19 -0500

    added BlueFirstAward class

---
+++ src/ProviderQuality.Console/Award/BlueFirstAward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class BlueFirstAward : AwardBase {
+        public override string Name { get { return "Blue First"; } }
+
+        public BlueFirstAward(int initialQuality, TimeSpan expirationTime) : base(initialQuality, expirationTime) {
+        }
+    }
+}
