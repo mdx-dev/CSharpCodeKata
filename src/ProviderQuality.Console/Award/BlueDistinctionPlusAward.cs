fa492acb92232bab5c9bb0868eaec2680d0a7325
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:46:44 -0500

    added BlueDistinctionPlusAward class

---
+++ src/ProviderQuality.Console/Award/BlueDistinctionPlusAward.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class BlueDistinctionPlusAward : AwardBase {
+        public override string Name { get { return "Blue Distinction Plus"; } }
+
+        private static readonly int InitialQuality = 80;
+
+        public BlueDistinctionPlusAward(TimeSpan expirationTime) : base(InitialQuality, expirationTime) {
+        }
+
+        // TODO: consider throwing here
+        //
+        new public int ChangeQuality(int step) {
+            return this.Quality;
+        }
+    }
+}
