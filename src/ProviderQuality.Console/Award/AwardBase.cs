4ff6910c113a876bb69dafe07fe098efa2f7ec29
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:37:52 -0500

    added AwardBase

---
+++ src/ProviderQuality.Console/Award/AwardBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    abstract public class AwardBase : IAward {
+        abstract public string Name { get; }
+        
+        public int Quality { get; protected set; }
+        public int DaysUntilExpiration { get; private set; }
+        public bool IsExpired { get { return DaysUntilExpiration < 0; }  }
+        public bool IsNotExpired { get { return !this.IsExpired; } }
+
+        public AwardBase(int initialQuality, TimeSpan expirationTime) {
+            this.Quality = initialQuality;
+            this.DaysUntilExpiration = expirationTime.Days;
+
+            if (this.Quality < 0)              this.Quality = 0;
+            if (this.DaysUntilExpiration < -1) this.DaysUntilExpiration = -1;
+        }
+
+        public int ChangeQuality(int step) {
+            this.Quality += step;
+            if (this.Quality > 50) this.Quality = 50;
+            if (this.Quality < 0 ) this.Quality = 0;
+            return this.Quality;
+        }
+
+        public int IncrementDay() {
+            if (this.DaysUntilExpiration > -1)
+                this.DaysUntilExpiration--;
+            return this.DaysUntilExpiration;
+        }
+    }
+}
