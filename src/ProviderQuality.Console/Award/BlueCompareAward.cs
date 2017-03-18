87f3923e80a092ffcb2d6fa121eb5c5bb2a147b3
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:50:20 -0500

    added BlueCompareAward class

---
+++ src/ProviderQuality.Console/Award/BlueCompareAward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award {
+    public class BlueCompareAward : AwardBase {
+        public override string Name { get { return "Blue Compare"; } }
+
+        public BlueCompareAward(int initialQuality, TimeSpan expirationTime) : base(initialQuality, expirationTime) {
+        }
+    }
+}
