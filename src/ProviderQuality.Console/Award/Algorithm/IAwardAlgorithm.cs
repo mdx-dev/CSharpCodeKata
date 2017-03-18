5c548248fcdb991d05ddd1e53bf0a9ee4ca5ebcd
Michael Reiland <Michael.S.Reiland@gmail.com>
2017-03-17 20:55:17 -0500

    added IAwardAlgorithm interface

---
+++ src/ProviderQuality.Console/Award/Algorithm/IAwardAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderQuality.Console.Award.Algorithm {
+    public interface IAwardAlgorithm {
+        IAward Update(IAward award);
+    }
+}
